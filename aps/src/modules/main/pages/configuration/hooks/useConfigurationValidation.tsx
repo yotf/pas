8ad7fb9fda6d250bea0c8fdc61685ffd3f5be956
tara.petric <tp@helveticode.ch>
2023-09-13 09:20:57 +0200

    reload configuration after save

--- aps/src/modules/main/pages/configuration/hooks/useConfigurationValidation.tsx
+++ aps/src/modules/main/pages/configuration/hooks/useConfigurationValidation.tsx
@@ -1,12 +1,13 @@
 import { UseFormReturn } from 'react-hook-form';
 import { ConfigurationFormData } from '../interfaces';
 import { useTranslate } from '@/modules/shared/hooks/translate.hook';
-import { useAppSelector } from '@/store/hooks';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { useEffect } from 'react';
 import {
   notificationFail,
   notificationSuccess,
 } from '@/modules/shared/services/notification.service';
+import { getConfiguration } from '../../settings/redux/configuration/thunks';
 export type ConfigurationValidationProps = {
   ns: string;
   form: UseFormReturn<ConfigurationFormData, any>;
@@ -18,6 +19,7 @@ export const useConfigurationValidation = ({ ns, form }: ConfigurationValidation
   } = form;
   const { translate } = useTranslate({ ns, keyPrefix: 'validation' });
   const { loading, error } = useAppSelector((state) => state.configuration);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (!isSubmitted || loading) return;
@@ -27,5 +29,6 @@ export const useConfigurationValidation = ({ ns, form }: ConfigurationValidation
       return;
     }
     notificationSuccess(translate('create_success'));
+    dispatch(getConfiguration()); 
   }, [error, loading, isSubmitted, reset, translate]);
 };
